5e2d11229ae6811ffe5f6d74c498b6084fe7d1c1
Nathã Emanuel <92585791+NathaEmanuel17@users.noreply.github.com>
2024-07-15 22:43:10 -0300

    first commit

---
+++ resources/js/Components/ListingAddress.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <span>
+            {{ listing.street }} {{ listing.street_nr }} {{ listing.city }}, for {{ listing.price }}
+        </span>
+    </div>
+</template>
+
+<script setup>
+ defineProps({
+    listing: Object
+ })
+</script>
---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,23 @@
+<template>
+    <Link href="/listing">Listings</Link>&nbsp;
+    <Link href="/listing/create">New Listing</Link>
+    <div v-if="flashSuccess" class="success">
+        {{ flashSuccess }}
+    </div>
+    <slot>Default</slot>
+</template>
+
+<script setup>
+import { computed } from 'vue' 
+import { Link, usePage } from '@inertiajs/vue3'
+
+const page = usePage()
+const flashSuccess = computed(() => page.props.flash.success)
+</script>
+
+<style scoped>
+.success {
+    background-color: green;
+    color: white;
+}
+</style>
